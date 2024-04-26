d228de7f9256e5a5499b48b88862010d4056f974
Ayush Sachan <sachan.ayush47@gmail.com>
2024-04-26 12:55:04 +0530

    feat: streaming

--- whisperai/WhisperAI.py
+++ whisperai/WhisperAI.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 import os
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import torch
 
 from whisperai.helpers.utils import get_env
 
@@ -12,47 +13,75 @@ class WhisperAI:
         self.model_size = model_size
         self.device = device
         self.compute_type = compute_type   
-        
-    def transcribe(self, audio: str, **kwargs):
-        transcription_model = WhisperModel(self.model_size, device=self.device, compute_type=self.compute_type)
-        
+        self.transcription_model = WhisperModel(self.model_size, device=self.device, compute_type=self.compute_type)
+        self.diarization_model = Pipeline.from_pretrained('pyannote/speaker-diarization-3.1', use_auth_token=get_env('HF_AUTH_TOKEN'))
+        self.diarization_model.to(torch.device(self.device))
+
+    def __transcriber(self, audio: str, **kwargs):
         vad_filter = kwargs.get('vad_filter', True)
         min_silence_duration_ms = kwargs.get('min_silence_duration_ms', 1000)
         
         options = dict(
-            word_timestamps=True,
+            word_timestamps=kwargs.get('word_timestamps', True),
             language=kwargs.get('language', None),
             initial_prompt=kwargs.get('initial_prompt', None),
-            task=kwargs.get('task', None),
+            task=kwargs.get('task', 'transcribe'),  # Accepted tasks: transcribe, translate
             temperature=kwargs.get('temperature', 0)
         )
         
-        segments, _ = transcription_model.transcribe(
+        segments, transcript_meta_info = self.transcription_model.transcribe(
             audio,
             vad_filter=vad_filter,
             vad_parameters=dict(min_silence_duration_ms=min_silence_duration_ms),
             **options
         )
         
-        segments = list(segments)  # The transcription will actually run here.
-        return segments
+        return segments, transcript_meta_info
 
+
+    def transcribe(self, audio: str, **kwargs):
+        options = dict(
+            word_timestamps=False,
+            min_silence_duration_ms = kwargs.get('min_silence_duration_ms', 1000),
+            vad_filter = kwargs.get('vad_filter', True),
+            language=kwargs.get('language', None),
+            initial_prompt=kwargs.get('initial_prompt', None),
+            task=kwargs.get('task', 'transcribe'),  # Accepted tasks: transcribe, translate
+            temperature=kwargs.get('temperature', 0)
+        )
+        
+        segments, transcript_meta_info = self.__transcriber(audio, **options)
+        
+        transcript = ''
+        for segment in segments:
+            transcript += segment.text + ' '
+        
+        return transcript
+    
         
     def diarize(self, audio: str, **kwargs):
+        """
+        Segments the audio into speaker turns.
+        It uses pyannote's speaker diarization model.
+        Currently, it only supports 2 speakers, it will be extended to support more speakers in the future updates.
+        """
+        
         OUTPUT_DIRECTORY = 'metadata'
         os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
         
+        dual_channel = kwargs.get('dual_channel', False)
+        
         options = dict(
+            word_timestamps=True,
             language = kwargs.get('language', None),
             initial_prompt = kwargs.get('initial_prompt', None),
-            task = kwargs.get('task', None),
+            task = kwargs.get('task', 'transcribe'),
             temperature = kwargs.get('temperature', 0),
             vad_filter = kwargs.get('vad_filter', True),
             min_silence_duration_ms = kwargs.get('min_silence_duration_ms', 1000)
         )
         
-        diarization_model = Pipeline.from_pretrained('pyannote/speaker-diarization-3.1', use_auth_token=get_env('HF_AUTH_TOKEN'))
-        diarization = diarization_model(audio, min_speakers=1, max_speakers=2)
+        diarization = self.diarization_model(audio, min_speakers=1, max_speakers=2)
         diarization_list = list(diarization.itertracks(yield_label=True))
 
         output = {
@@ -99,8 +128,8 @@ class WhisperAI:
         # stereo_audio = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
         # stereo_audio.export(f"{base_file_name}_stereo.mp3", format="mp3")
         
-        segments_left = self.transcribe(left_channel_path, **options)
-        segments_right = self.transcribe(right_channel_path, **options)
+        segments_left, transcript_meta_info_left = self.__transcriber(left_channel_path, **options)
+        segments_right, transcript_meta_info_right = self.__transcriber(right_channel_path, **options)
 
         def get_words(segments, speaker):
             words = []
@@ -134,14 +163,24 @@ class WhisperAI:
       
       
     def stream(self, audio: str, **kwargs):
+        """
+        Streams the segment's text as it is transcribed.
+        It does not wait for the entire transcription to finish.
+        It does not support diarization. 
+        """
+        
         options = dict(
-            word_timestamps=True,
+            word_timestamps=False,
+            vad_filter = kwargs.get('vad_filter', True),
+            min_silence_duration_ms = kwargs.get('min_silence_duration_ms', 1000),
             language=kwargs.get('language', None),
             initial_prompt=kwargs.get('initial_prompt', None),
-            task=kwargs.get('task', None),
-            temperature=kwargs.get('temperature', 0),
-            vad_filter = kwargs.get('vad_filter', True),
-            min_silence_duration_ms = kwargs.get('min_silence_duration_ms', 1000)
+            task=kwargs.get('task', 'transcribe'),  # Accepted tasks: transcribe, translate
+            temperature=kwargs.get('temperature', 0)
         )
         
+        segments, transcript_meta_info = self.__transcriber(audio, **options)
+        
+        for segment in segments:
+            yield segment.text
         
