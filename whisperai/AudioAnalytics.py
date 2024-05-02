acf02339fcd879a4bf0df2545a705bdcab044ce4
Ayush Sachan <sachan.ayush47@gmail.com>
2024-05-02 19:47:03 +0530

    feat: audio analytics

--- whisperai/AudioAnalytics.py
+++ whisperai/AudioAnalytics.py
@@ -1,23 +1,33 @@
 from faster_whisper import WhisperModel
+from pydub.utils import mediainfo
 
 class AudioAnalytics:
-    def __init__(self, audio_path: str, model_size: str, device=None, compute_type=None) -> None:
+    def __init__(self, audio_path: str, model_size: str, device=None, compute_type=None, **kwargs) -> None:
         self.__transcription_model = WhisperModel(model_size, device=device, compute_type=compute_type)
         self.__audio_path = audio_path
-        self.language = None
-        self.dead_air_duration = None
-        self.sampling_rate = None
-        self.duration_after_vad = None
-        self.duration = None
         
-    def get_language(self):
-        pass
-    
-    def get_dead_air_duration(self):
-        pass
-    
-    def get_sampling_rate(self):
-        pass
-    
-    def get_duration_after_vad(self):
-        pass
+        vad_filter = kwargs.get('vad_filter', True)
+        min_silence_duration_ms = kwargs.get('min_silence_duration_ms', 1000)
+        temperature=kwargs.get('temperature', 0)
+        
+        if not kwargs.get('language', None) or not kwargs.get('duration_after_vad', None) or not kwargs.get('duration', None):
+            segments, transcript_meta_info = self.__transcription_model(
+                self.__audio_path,
+                temperature=temperature,
+                vad_filter=vad_filter,
+                vad_parameters=dict(min_silence_duration_ms=min_silence_duration_ms)
+            )
+        
+            self.language = transcript_meta_info.language
+            self.duration_after_vad = transcript_meta_info.duration_after_vad   # In seconds
+            self.duration = transcript_meta_info.duration   # In seconds
+        else:
+            self.language = kwargs['language']
+            self.duration_after_vad = kwargs['duration_after_vad']
+            self.duration = kwargs['duration']
+            
+        audio_metadata = mediainfo(self.__audio_path)
+        self.sample_rate = audio_metadata['sample_rate']
+        self.channels = audio_metadata['channels']
+        self.bit_rate = audio_metadata['bit_rate']
+        self.codec_name = audio_metadata['codec_name']
--- whisperai/Transcript.py
+++ whisperai/Transcript.py
@@ -2,7 +2,6 @@ class Transcript:
     def __init__(self, transcript, **kwargs):
         self.transcript = transcript
         # self.raw_transcript = transcript
-        # self.diarized_transcript = self.diarize_transcript(transcript)
         # self.profanity = self.get_profanity()
         # self.summary = self.get_summary()
         # self.language = self.get_language()
@@ -13,18 +12,6 @@ class Transcript:
         pass
     
     def get_sentiment(self):
-        pass
-    
-    def get_language(self):
-        pass
-    
-    def get_transcript(self):
-        pass
-    
-    def get_raw_transcript(self):
-        pass
-    
-    def get_diarized_transcript(self):
         pass
     
     def get_summary(self):
